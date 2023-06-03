f02c98e5d30387eb501dccbf5df9188e3f01d42a
Daniel Anugerah <danielanugerah2004@gmail.com>
2023-06-03 18:11:14 +0800

    implement block

---
+++ block/block_test.go
@@ -0,0 +1,33 @@
+package block
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewBlock(t *testing.T) {
+	testcases := []struct {
+		name string
+		fn   func(t *testing.T)
+	}{
+		{
+			name: "NewBlock",
+			fn: func(t *testing.T) {
+				bc := NewBlockchain()
+				bc.AddBlock("Send 1 BTC to Ivan")
+				bc.AddBlock("Send 2 more BTC to Ivan")
+
+				assert.Equal(t, 3, len(bc.blocks), "NewBlock() = %v, want %v", len(bc.blocks), 3)
+
+				for _, block := range bc.blocks {
+					t.Logf("Prev. hash: %x\n", block.PrevBlockHash)
+					t.Logf("Data: %s\n", block.Data)
+					t.Logf("Hash: %x\n", block.Hash)
+				}
+			},
+		},
+	}
+	for _, tc := range testcases {
+		t.Run(tc.name, tc.fn)
+	}
+}
